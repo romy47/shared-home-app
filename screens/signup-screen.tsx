b723f5e705b075fab740ccf297240ad090bdc71a
Sebastian Gomes <romy6047@gmail.com>
2024-12-08 17:22:56 -0600

    google signin implemented

--- screens/signup-screen.tsx
+++ screens/signup-screen.tsx
@@ -3,7 +3,14 @@ import React, { useState } from "react";
 import { useNavigation } from '@react-navigation/native';
 import { AppleButton, appleAuth } from '@invertase/react-native-apple-authentication';
 import auth from '@react-native-firebase/auth';
-
+import {
+    GoogleSignin,
+    isErrorWithCode,
+    isSuccessResponse,
+    statusCodes,
+    GoogleSigninButton
+  } from '@react-native-google-signin/google-signin';
+  
 async function onAppleButtonPress() {
     // Start the sign-in request
     const appleAuthRequestResponse = await appleAuth.performRequest({
@@ -24,12 +31,59 @@ async function onAppleButtonPress() {
   
     // Sign the user in with the credential
     return auth().signInWithCredential(appleCredential);
-  }
+}
+// import statusCodes along with GoogleSignin
 
+  // Somewhere in your code
+async function onGoogleButtonPress() {
+    try {
+        // Check if your device supports Google Play
+        await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+        // Get the users ID token
+        const signInResult = await GoogleSignin.signIn();
+
+        // Try the new style of google-sign in result, from v13+ of that module
+        let idToken = signInResult.data?.idToken;
+        // if (!idToken) {
+        //     // if you are using older versions of google-signin, try old style result
+        //     idToken = signInResult.idToken;
+        // }
+        if (!idToken) {
+            throw new Error('No ID token found');
+        }
+
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(googleCredential);
+    } catch (error) {
+        if (isErrorWithCode(error)) {
+        switch (error.code) {
+            case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break;
+            case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // Android only, play services not available or outdated
+            break;
+            default:
+            // some other error happened
+        }
+        } else {
+        // an error that's not related to google sign in occurred
+        }
+    }
+};
+  
+  
 export default function SignupScreen() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+
+    GoogleSignin.configure({
+        webClientId: '459808310970-j4kklrta39an8t8kvjb4ijli5e07rfvp.apps.googleusercontent.com',
+    });
 
     const navigation = useNavigation();
     const handleSignUp = () => {
@@ -100,7 +154,10 @@ export default function SignupScreen() {
                     >
                         <Text style={styles.linkText}>Already have an account? Sign In</Text>
                     </TouchableOpacity>
-
+                    <GoogleSigninButton
+                        size={GoogleSigninButton.Size.Wide}
+                        onPress={onGoogleButtonPress}
+                    />
                     <AppleButton
                         buttonStyle={AppleButton.Style.BLACK}
                         buttonType={AppleButton.Type.SIGN_IN}
